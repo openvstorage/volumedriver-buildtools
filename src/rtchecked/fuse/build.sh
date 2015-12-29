5b6fd7a7a846da18954f21633338f18c286226d0
Arne Redlich <arne.redlich@openvstorage.com>
2015-12-29 12:59:19 +0100

    fuse: the upstream now lives on github

--- src/rtchecked/fuse/build.sh
+++ src/rtchecked/fuse/build.sh
@@ -4,23 +4,22 @@ set -eux
 . ${VOLUMEDRIVER_BUILD_CONFIGURATION?"You need to set the path to the build configuration file"}
 
 
-FUSE_VERSION=fuse-2.9.3
-FUSE_DIR=$FUSE_VERSION
-FUSE_PACKAGE=$FUSE_VERSION.tar.gz
-FUSE_BUILD_DIR=fuse_build
-FUSE_URL=http://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/$FUSE_PACKAGE
-
+FUSE_VERSION=2.9.3
+FUSE_DIR=fuse-$FUSE_VERSION
+FUSE_PACKAGE=fuse-$FUSE_VERSION.tar.gz
+FUSE_BUILD_DIR=${FUSE_VERSION}.build
+# upstream glitch - all tarballs are currently under a "fuse_2_9_4" dir instead of
+# fuse_${FUSE_VERSION//"."/"_"}
+FUSE_URL=https://github.com/libfuse/libfuse/releases/download/fuse_2_9_4/${FUSE_PACKAGE}
 
 . ../definitions.sh
 . ../../helper-functions.sh
 
 get_package_or_die $FUSE_PACKAGE $FUSE_URL
 
-
 echo ">+>+> Cleaning up from previous build"
 rm -rf $FUSE_DIR $FUSE_BUILD_DIR
 echo "<+<+< Done cleaning up from previous build"
-
 
 echo ">+>+> Unpacking $FUSE_PACKAGE"
 tar -xzf $SOURCES_DIR/$FUSE_PACKAGE
@@ -33,7 +32,6 @@ patch -p1 < ../fuse-unlink-directories-with-unlinked-open-files.patch
 patch -p1 < ../fuse-introduce-a-means-to-control-the-threads-employed-by.patch
 popd
 echo "<+<+< Done patching $FUSE_PACKAGE"
-
 
 mkdir $FUSE_BUILD_DIR
 pushd $FUSE_BUILD_DIR
@@ -48,7 +46,6 @@ CFLAGS="$C_COMPILER_FLAGS" CC=$C_COMPILER ../$FUSE_DIR/configure \
     --disable-shared
 echo "<+<+< Done configuring $FUSE_VERSION"
 
-
 echo ">+>+> Building $FUSE_VERSION"
 make -j ${BUILD_NUM_PROCESSES-2}
 echo "<+<+< Done building $FUSE_VERSION"
@@ -59,7 +56,6 @@ echo "Build used $BUILD_SIZE disk space"
 echo ">+>+> Installing $FUSE_VERSION"
 make install
 echo "<+<+< Done installing $FUSE_VERSION"
-
 
 echo ">+>+>+ Cleaning up"
 popd
