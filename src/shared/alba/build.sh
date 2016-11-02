ead3cd19c755b6015c0b1359a68c99b5f8d335a0
Arne Redlich <arne.redlich@openvstorage.com>
2016-11-02 15:31:05 +0100

    Alba: update to version with extend-int32-to-int64 support
    Happy 1.0.0, Alba.

--- src/shared/alba/build.sh
+++ src/shared/alba/build.sh
@@ -2,7 +2,7 @@
 set -eux
 . ${VOLUMEDRIVER_BUILD_CONFIGURATION?"You need to set the path to the build configuration file"}
 
-ALBA_VERSION=0.9.22
+ALBA_VERSION=1.0.0
 ALBA_DIR=../../../../alba.git
 
 . ../definitions.sh
