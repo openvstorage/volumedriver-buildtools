55016cbfef5a2a8142438c44eb67149230219233
Arne Redlich <arne.redlich@openvstorage.com>
2017-01-12 21:05:37 +0100

    alba: client lib with reworked MessageBuilder

--- src/shared/alba/build.sh
+++ src/shared/alba/build.sh
@@ -2,7 +2,7 @@
 set -eux
 . ${VOLUMEDRIVER_BUILD_CONFIGURATION?"You need to set the path to the build configuration file"}
 
-ALBA_VERSION=1.3.3
+ALBA_VERSION=1.3.4
 ALBA_DIR=../../../../alba.git
 
 . ../definitions.sh
